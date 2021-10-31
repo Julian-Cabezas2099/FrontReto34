fe9f84338cc2fd914d6d09b770ff34accc59fc50
srg77 <samuel-rodriguez2@upc.edu.co>
2021-10-30 20:31:50 -0500

    página de reportes terminada

--- js/reports.js
+++ js/reports.js
@@ -1,30 +1,60 @@
+function consultarRepClientes(){
 
+    $.ajax(
+        {
+            url         : 'http://localhost:8080/api/Reservation/report-clients',
+            type        : 'GET',
+            dataType    : 'JSON',
+            success     : function(respuesta){
+                console.log(respuesta);
+                    $("#tablaid").show();
+                    for (let i = 0; i < respuesta.length; i++) {
+                        
+                        $("#tablaReporte").append("<tr>");
+                        $("#tablaReporte").append("<td class='text-center'>" + respuesta[i].client.name + "</td>");
+                        $("#tablaReporte").append("<td class='text-center'>" + respuesta[i].client.email + "</td>");
+                        $("#tablaReporte").append("<td class='text-center'>" + respuesta[i].total + "</td>");
+                        $("#tablaReporte").append("</tr>");
+                        
+                    }
+                        
 
-function consultar() {
-    var f = new Date(document.getElementById("fechaInicio").value);
-    var fechaInicio = f.getFullYear()+ '-' + ( f.getMonth() + 1 ) + '-' + f.getDate();
-    var f2 = new Date(document.getElementById("fechaEntrega").value);
-    var fechaEntrega = f2.getFullYear()+ '-' + ( f2.getMonth() + 1 ) + '-' + f2.getDate();
-    console.log("antes del ajax");
+            }
+        }
+    );
+}
+
+function consultarConReservas() {
+    // var f1 = new Date(document.getElementById("fechaInicio").value);
+    let f1 = new Date($("#fechaInicio").val());
+    let isoF = f1.toISOString();
+    let fechaInicio = isoF.slice(0,10);
+    // var fechaInicio = (f.getFullYear())+ '-' + ( f.getMonth() + 1 ) + '-' + f.getDate();
+    let f2 = new Date($("#fechaEntrega").val());
+    // var f2 = new Date(document.getElementById("fechaEntrega").value);
+    let isoFEntrega = f2.toISOString();
+    let fechaEntrega = isoFEntrega.slice(0,10);
+    // var fechaEntrega = (f2.getFullYear())+ '-' + ( f2.getMonth() + 1 ) + '-' + f2.getDate();
     $.ajax(
              {
-                url          : 'localhost:8080/api/Reservation/report-dates/'+fechaInicio+'/'+fechaEntrega,
+                url          : 'http://localhost:8080/api/Reservation/report-dates/'+fechaInicio+'/'+fechaEntrega,
                 type         : 'GET',
                 dataType     : 'JSON',
                 success      :  function(json){
-                    console.log("antes del for");
+            
                                     $("#resultado").empty();
-                                    var contador = 0;
-                                    document.getElementById("tablaid").style.display="";
+                                    var numElementos = json.length
+                                    // var contador = 0;
                                     
-                                    for (i=0; i < json.length; i++){
+                                    
+                                    // for (i=0; i < json.length; i++){
 
-                                       // var idClient= json[i].idClient;
-                                        //var nombre = json[i].name;  
-                                        contador++;                
+                                    //    // var idClient= json[i].idClient;
+                                    //     //var nombre = json[i].name;  
+                                    //     contador++;                
 
-                                    }
-                                    $("#resultado").val() = "TOTAL DE RESERVAS EN EL TIEMPO SELECCIONADO: "+contador;
+                                    // }
+                                    $("#resultado").append("TOTAL DE RESERVAS EN EL TIEMPO SELECCIONADO: "+numElementos);
 
                                     console.log(json)
                                 },
@@ -34,4 +64,24 @@ function consultar() {
                                 }
              }
     );    
+}
+
+function consultarRepVs (){
+
+    $.ajax(
+        {
+            url         : 'http://localhost:8080/api/Reservation/report-status',
+            type        : 'GET',
+            dataType    : 'JSON',
+            success     : function(respuesta){
+                console.log(respuesta);
+                    $("#contenedorRepVs").show();
+                    $("#resCompleted").append("<p>Reservas completadas: "+respuesta.completed+"</p>");
+                    $("#resCancelled").append("<p>Reservas canceladas: "+respuesta.cancelled+"</p>");
+                        
+
+            }
+        }
+    );
+
 }
